c0006b230628febceac71094fb7bdd85a43d7717
Richard Kooijman <rkooijman@quicknet.nl>
2021-02-01 23:04:59 +0100

    made a generic setup for PUT's of bundles

--- app/sap.py
+++ app/sap.py
@@ -4,14 +4,26 @@ from datetime import datetime
 import threading
 import connexion
 from connexion import NoContent
+import inspect
 
+
+# base directory where to put data
 DATA_DIR = "./data"
 
-def put_articles():
+
+def put_bundle():
+	# bundle name is the name of the function of the caller of the function that triggered this one,
+	# without the first part before the first '_' character
+	# so with this stack, caller1() -> do_caller2() -> put_bundle(), "do_caller2" will be returned
+	bundle_name = (inspect.stack()[1][3]).split("_", 1)[1]
+	# get the thread identifier to keep bundles apart that are written to in different threads
 	id = threading.get_ident()
 	now = datetime.now().strftime("%Y%m%d_%H%M%S")
-	filename = f"articles_{id}_{now}.csv"
-	with open(os.path.join(DATA_DIR, filename), 'w', newline='') as csvfile:
+	filename = f"{bundle_name}_{id}_{now}.csv"
+	directory = os.path.join(DATA_DIR, bundle_name)
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	with open(os.path.join(directory, filename), 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile)
 		body = connexion.request.json
 		print(body)
@@ -21,41 +33,63 @@ def put_articles():
 			if count == 0:
 				writer.writerow(row.keys())
 			writer.writerow(row.values())
-	return NoContent, (200 if body is not None else 201)
+			count += 1
+	return NoContent, 201
+
+
+def put_articles():
+	return put_bundle()
+
+
+# def put_articles():
+# 	id = threading.get_ident()
+# 	now = datetime.now().strftime("%Y%m%d_%H%M%S")
+# 	filename = f"articles_{id}_{now}.csv"
+# 	with open(os.path.join(DATA_DIR, filename), 'w', newline='') as csvfile:
+# 		writer = csv.writer(csvfile)
+# 		body = connexion.request.json
+# 		print(body)
+# 		count = 0
+# 		for row in body:
+# 			print(list(row.keys()))
+# 			if count == 0:
+# 				writer.writerow(row.keys())
+# 			writer.writerow(row.values())
+# 			count += 1
+# 	return NoContent, 201
+
 
 def put_article_valuations():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_article_conditiontypes():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_article_preferred_suppliers():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_locations():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_receipts():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_stock_levels():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_stock_mutations():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_stockrooms():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
+	return NoContent, 201
+
 
 def put_suppliers():
-	p = ''
-	return NoContent, (200 if p is not None else 201)
-
+	return NoContent, 201
