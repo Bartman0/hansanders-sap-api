8a6c5ce48c16996bdc87ee394bb583bd0bef93a6
Richard Kooijman <rkooijman@quicknet.nl>
2021-02-01 18:09:31 +0100

    back to dev server

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import connexion
 
-app = connexion.FlaskApp(__name__, specification_dir='.', server='tornado')
+#app = connexion.FlaskApp(__name__, specification_dir='.', server='tornado')
+app = connexion.FlaskApp(__name__, specification_dir='.')
 app.add_api('swagger.yaml')
 print(app.app.__dict__)
 app.run(port=8080)
