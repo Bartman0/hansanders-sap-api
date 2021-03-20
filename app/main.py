5f2b9a3e2396a3498d6212a24ac28928a3cd63f8
Richard Kooijman <rkooijman@quicknet.nl>
2021-03-20 16:28:20 +0100

    added examples added basic auth

--- app/main.py
+++ app/main.py
@@ -1,4 +1,25 @@
 import connexion
+from connexion.decorators.security import validate_scope
+from connexion.exceptions import OAuthScopeProblem
+
+
+def basic_auth(username, password, required_scopes=None):
+    if username == 'sap' and password == 'secret':
+        info = {'sub': 'admin', 'scope': ''}
+    else:
+        # optional: raise exception for custom error response
+        return None
+
+    # optional
+    if required_scopes is not None and not validate_scope(required_scopes, info['scope']):
+        raise OAuthScopeProblem(
+                description="Provided user doesn't have the required access rights",
+                required_scopes=required_scopes,
+                token_scopes=info['scope']
+            )
+
+    return info
+
 
 #app = connexion.FlaskApp(__name__, specification_dir='.', server='tornado')
 app = connexion.FlaskApp(__name__, specification_dir='.')
